5aa58d37ceb47b59b4f82d5a0f9fe3cc8db36aa3
Steven <scates09@gmail.com>
2017-06-21 20:04:18 -0500

    changed addNetworkPrinter to make it actually work, ya know

--- addNetworkPrinter.sh
+++ addNetworkPrinter.sh
@@ -6,21 +6,32 @@ CUPSTATUS=$(/etc/init.d/cups status | grep '(running)')
 read -p "Enter a name for the printer (NO SPACES): " PRINTERNAME
 echo
 read -p "Enter the IP address of the printer (e.g. 10.1.10.150): " PRINTERIP
+
+ping -q -c 1 -w 2 $PRINTERIP > /dev/null
+if [ $? -ne 0 ]; then
+	echo
+	echo "COULD NOT REACH PRINTER OVER NETWORK."
+	echo "PLEASE CHECK CONNECTION AND RUN SCRIPT AGAIN."
+	sleep 3
+	exit
+fi
+
 echo
 read -p "Would you like to set this printer as default (y/N): " SETDEFAULT
 echo
 
 echo "Making sure CUPS service is running."
-if [ ! "$CUPSTATUS" ]; then
+if [ -z "$CUPSTATUS" ]; then
+	echo
 	sudo /etc/init.d/cups start
 fi
 
-echo "Adding printer @ selected IP."
-lpadmin -p "$PRINTERNAME" -E -v socket://$PRINTERIP
+echo "Adding printer at selected IP."
+lpadmin -p $PRINTERNAME -E -v socket://$PRINTERIP
 
 PRINTERSTATUS=$(lpstat -p -d | grep "$PRINTERNAME")
 
-if [ ! "$PRINTERSTATUS" ]; then
+if [ -z "$PRINTERSTATUS" ]; then
 	echo
 	echo "ERROR OCCURED! PRINTER NOT ADDED!"
 	sleep 3
@@ -42,7 +53,7 @@ if [ "$SETDEFAULT" == "y" -o "$SETDEFAULT" == "Y" ] ; then
 fi
 
 echo "Printing test page."
-lpr -p "$PRINTERNAME" /usr/share/cups/data/topsecret.pdf
+lpr -P $PRINTERNAME /usr/share/cups/data/testprint
 echo "Printing can take up to 60 seconds."
 echo
 echo "CONGRATULATIONS. PRINTER SETUP COMPLETE!"
